3f1e74bd5e3eb28964a6d516c73e0be1195f6dbc
ChiaraAuriemma <chiara.auriemma@mail.polimi.it>
2023-08-02 12:01:29 +0200

    Merge branch 'main' of https://github.com/jakeeuu/TIW-project into main

--- code/RIA-project/src/main/webapp/javascript/management.js
+++ code/RIA-project/src/main/webapp/javascript/management.js
@@ -78,7 +78,7 @@
 			 * CART PAGE
 			 * 
 			 */
-			visualizeCartProduct = new VisualizeCartProduct(alert, document.getElementById("cartTable"),document.getElementById("cartBody"));
+			visualizeCartProduct = new VisualizeCartProduct(alert, document.getElementById("cartTable"),document.getElementById("cartBody"),this);
 			visualizeCartProduct.reset();
 			/**
 			 * 
@@ -860,10 +860,11 @@
 	}
 
 
-	function VisualizeCartProduct(alertIn, containerIn, bodyIn){
+	function VisualizeCartProduct(alertIn, containerIn, bodyIn, orchestratorIn){
 		this.alert = alertIn;
 		this.container = containerIn;
 		this.body = bodyIn;
+		this.orchestrator = orchestratorIn;
 
 		this.reset = function() {
 			this.container.style.visibility = "hidden";
